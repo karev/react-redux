399bdba440bb5dc89869dec4d44fdd8a50e2131c
abdelwahed mohamed <abdelwahed.mohamed@outlook.com>
2016-11-08 17:19:59 +0100

    add header

---
+++ src/components/common/Header/Header.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react';
+import { Link, IndexLink } from 'react-router';
+
+const Header = (props) => {
+    return (
+        <nav>
+            <IndexLink to="/" activeClassName="active">Accueil</IndexLink>
+            {" | "}
+            <Link to="about" activeClassName="active">About</Link>
+        </nav>
+    );
+};
