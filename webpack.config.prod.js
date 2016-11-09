61ff8592a13703cfe48e69e1e1e9500a43cdc14a
abdelwahed mohamed <abdelwahed.mohamed@outlook.com>
2016-11-09 19:16:40 +0100

    polish prod build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -33,16 +33,20 @@ export default {
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
-      }
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
     })
   ],
   module: {
     loaders: [
       {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
       {test: /(\.css)$/, loader: ExtractTextPlugin.extract('css-loader!postcss-loader')},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file?name=fonts/[name].[ext]'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000&name=fonts/[name].[ext]'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]'},
       {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
       {test: /\.json$/, loader: 'json-loader'},
       {test: /\.(png|jpg)$/, loader: 'url?limit=10000&name=img/[name].[ext]'}
